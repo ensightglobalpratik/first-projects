9c8da4b11742603ae40568dea7c61ea31e402c77
ensightglobalpratik <pratik.d@ensightglobal.com>
2019-01-31 17:38:40 +0530

    remove sample.php

--- sample.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <head>
-        <title></title>
-        <link rel="stylesheet" type="text/css" href="style.css">
-    </head>
-    <body>
-        <h2>This my first example of git</h2>
-    </body>
-</html>
